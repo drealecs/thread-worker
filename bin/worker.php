8e2eae86e7cb4702e85ed7549a0ca794b0fefbe2
Alexandru Patranescu <alexandru.patranescu@payu.ro>
2014-01-09 16:18:33 +0200

    quit if worker.php does not have a queue_list parameter

--- bin/worker.php
+++ bin/worker.php
@@ -4,7 +4,8 @@ set_time_limit(0);
 (@include_once __DIR__ . '/../vendor/autoload.php') || @include_once __DIR__ . '/../../../autoload.php';
 
 if ($argc < 2) {
-    echo "Worker call must be: worker queue_list\n";
+    echo "Worker call must be with: worker queue_list\n";
+    die;
 }
 $type = $argv[1];
 $worker = new ThreadWorker\Worker(new ThreadWorker\RedisQueue($type));
