47f04d4f26d44fb86f8f26892ed8e3c64774f614
Alexandru Patranescu <alexandru.patranescu@payu.ro>
2014-01-09 14:49:30 +0200

    add worker.php as a binary file in composer.json

--- bin/worker.php
+++ bin/worker.php
@@ -1,8 +1,11 @@
 <?php
 set_time_limit(0);
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+(@include_once __DIR__ . '/../vendor/autoload.php') || @include_once __DIR__ . '/../../../autoload.php';
 
+if ($argc < 2) {
+    echo "Worker call must be: worker queue_list\n";
+}
 $type = $argv[1];
 $worker = new ThreadWorker\Worker(new ThreadWorker\RedisQueue($type));
 $worker->work();
