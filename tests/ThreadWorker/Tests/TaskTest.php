ef2b1318831580fb58cbb70be06e56ab573dc2ab
Alexandru Patranescu <alexandru.patranescu@payu.ro>
2014-01-10 14:52:51 +0200

    finalized Task class 100% coverage

--- tests/ThreadWorker/Tests/TaskTest.php
+++ tests/ThreadWorker/Tests/TaskTest.php
@@ -10,6 +10,24 @@ class TaskTest extends \PHPUnit_Framework_TestCase
             ->method('run')
             ->with('testParam1', 'testParam2')
             ->will($this->returnValue('testResult'));
+        
+        $this->assertEquals('testResult', $task());
+
+        $serializedTask = serialize($task);
+        $task = unserialize($serializedTask);
+        
+        $task->expects($this->once())
+            ->method('run')
+            ->with('testParam1', 'testParam2')
+            ->will($this->returnValue('testResult'));
+        
         $this->assertEquals('testResult', $task());
     }
+    
+    public function testTaskBadImplementation()
+    {
+        $this->setExpectedException('Exception');
+        $task = $this->getMock('ThreadWorker\Task');
+    }
+    
 } 
