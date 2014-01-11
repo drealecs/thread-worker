54c79cd8a01763b929fa20cbd89ad69403bda1ea
Alexandru Patranescu <drealecs@gmail.com>
2014-01-11 08:17:19 +0200

    task exception tests

---
+++ tests/ThreadWorker/Tests/TaskExceptionTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace ThreadWorker\Tests;
+
+use ThreadWorker\TaskException;
+
+class TaskExceptiontTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTaskExceptionEncapsulation()
+    {
+        $testMessage = 'testString';
+        $testCode = 17;
+
+        $innerException = new \Exception($testMessage, $testCode);
+
+        $exception = new TaskException($innerException);
+
+        $this->assertEquals($testMessage, $exception->getMessage());
+        $this->assertEquals($testCode, $exception->getCode());
+        $this->assertEquals(13, $exception->getLine());
+        $this->assertEquals('TaskExceptionTest.php', basename($exception->getFile()));
+
+        $serializedException = serialize($exception);
+        $exception = unserialize($serializedException);
+
+        $this->assertEquals($testMessage, $exception->getMessage());
+        $this->assertEquals($testCode, $exception->getCode());
+        $this->assertEquals(13, $exception->getLine());
+        $this->assertEquals('TaskExceptionTest.php', basename($exception->getFile()));
+    }
+} 
